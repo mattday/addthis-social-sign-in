b833e8131ff8d05620bff6c7a8a089da859b1d93
srijith.v <srijith.v@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-20 13:50:50 +0000

    Releasing version 2.0.1
    git-svn-id: https://plugins.svn.wordpress.org/addthis-social-sign-in/trunk@684783 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/linkedin.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('linkedin-connect').onclick = function (){	
-	var url = window.location.href.split('#')[0];
-	if (url.indexOf('?') > -1){
-		url += '&lType=initiate'
-	}else {
-		url += '?lType=initiate'
-	}
-	window.location.href = url;
-}
